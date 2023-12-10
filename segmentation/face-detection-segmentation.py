520233c78f9ab6a0646c96af1858d53da62620d4
Amr Elsheshtawy <96791769+Amreux@users.noreply.github.com>
2023-12-11 00:46:34 +0200

    modify: segmentation

--- segmentation/face-detection-segmentation.py
+++ segmentation/face-detection-segmentation.py
@@ -7,24 +7,23 @@ def segement(image):
 
 
     # initialize the mask
-    mask = np.ones(image.shape[:2], np.uint8)
+    mask = np.zeros(image.shape[:2], np.uint8)
 
     # not sure what these are for in the algorithm
     backgroundModel = np.zeros((1, 65), np.float64) 
     foregroundModel = np.zeros((1, 65), np.float64) 
 
 
-    # this is to initialize a rectangular mask to capture the core of the image
-    div=12
     img_height=image.shape[0]
     img_width= image.shape[1]
-    img_height_div=img_height//div
-    img_width_div=img_width//div
-    rectangle = (0,img_height_div,10*img_height_div,10*img_width_div)
+   
 
-    # run the grabcut algorithm for 2 iterations
-    cv2.grabCut(image, mask, rectangle, backgroundModel, foregroundModel, 5, cv2.GC_INIT_WITH_RECT) 
-    # cv2.grabCut(image, mask, rectangle, backgroundModel, foregroundModel, 2, cv2.GC_INIT_WITH_MASK) 
+   # initialize a rectangle for the grabcut algorithm
+    rectangle = (1,1,img_width,img_height)
+
+    # run the grabcut algorithm for 8 iterations
+    cv2.grabCut(image, mask, rectangle, backgroundModel, foregroundModel, 10, cv2.GC_INIT_WITH_RECT) 
+   
 
     # generate the forground mask
     mask_forground = np.where((mask == 2)|(mask == 0), 0, 1).astype('uint8') 
@@ -32,7 +31,10 @@ def segement(image):
     # get the final image
     result_img = image * mask_forground[:, :, np.newaxis] 
 
-    return result_img
+ 
+    result_mask = np.where((result_img>0),1,0).astype(float)
+
+    return result_mask
 
 
 
